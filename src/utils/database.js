d6513ed14f045ddcb1db112847be8c026f67c63e
david-lo <rcl920@hotmail.com>
2020-11-06 02:33:23 +0800

    spelling correct

--- src/utils/database.js
+++ src/utils/database.js
@@ -2,7 +2,7 @@ const { MongoClient } = require('mongodb')
 
 const _db = 'maindb'
 
-const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_password}@cluster0.ipfgb.${_db}.net/maindb?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_password}@cluster0.ipfgb.mongodb.net/${_db}?retryWrites=true&w=majority`
 
 exports.mongoInsertOne = async function (doc) {
     const mongoClient = new MongoClient (uri, { useNewUrlParser: true, useUnifiedTopology: true })
